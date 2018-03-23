ef6c426aad1e6a91e1390ce1631c69d8d1389e2a
satoken912 <seat.soil.74196901@gmail.com>
2018-03-23 15:59:39 +0900

    Add Canvas for Output

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function setup(){
-    createCanvas(600, 400);
-    background(128);
+var app_output = function(p){
+
+    p.setup = function(){
+        p.createCanvas(600, 400);
+        p.background(128, 128);
+    }
+
 }
+
+var canvas_output = new p5( app_output, "canvas_output" );
